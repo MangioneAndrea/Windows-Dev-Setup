ce70551dc1a4c4a19cfc84ff731cb332d8ec6f8e
Andrea Mangione <35958935+MangioneAndrea@users.noreply.github.com>
2023-06-29 13:50:17 +0200

    add some missing packages

---
+++ Nvim/index.ps1
@@ -0,0 +1,5 @@
+git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
+
+git clone https://github.com/MangioneAndrea/nvim-config "$env:LOCALAPPDATA\nvim"
+
+choco install ripgrep
